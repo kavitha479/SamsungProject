ec7db51e04cf64cefa315e35c6e5fc430f364fee
NewAccount <NewAccount@DESKTOP-JUFF927.Home>
2022-07-14 15:58:43 -0400

    Second commit

--- src/Sales/Samsung.java
+++ src/Sales/Samsung.java
@@ -5,6 +5,7 @@ public class Samsung {
 	public static void main(String[] args) {
 	System.out.println("Welcome to Samsung");
 	System.out.println("Samsung Electronics");
+	System.out.println(" Offers on AC");
 	
 
 	}
