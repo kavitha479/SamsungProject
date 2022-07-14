0256ae14468d6fe4a1dcb66a008731772aaee807
NewAccount <NewAccount@DESKTOP-JUFF927.Home>
2022-07-14 15:52:43 -0400

    First commit

---
+++ src/Sales/Samsung.java
@@ -0,0 +1,12 @@
+package Sales;
+
+public class Samsung {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Samsung");
+	System.out.println("Samsung Electronics");
+	
+
+	}
+
+}
